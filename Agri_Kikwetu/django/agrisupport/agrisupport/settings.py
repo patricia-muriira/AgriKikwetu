7cb8d7ad72880945f3d1581d3c906a6e436e4c68
Patricia Muriira <117148537+patricia-muriira@users.noreply.github.com>
2026-01-13 09:03:26 +0200

    Fix formatting and comments

--- Agri_Kikwetu/django/agrisupport/agrisupport/settings.py
+++ Agri_Kikwetu/django/agrisupport/agrisupport/settings.py
@@ -14,10 +14,9 @@ from pathlib import Path
 from dotenv import load_dotenv
 import os
 
-# Load environment variables from the .env file
+# Loading environment variables from the .env file
 load_dotenv()
 
-# Now you can access your environment variables using os.getenv
 TELEGRAM_TOKEN = os.getenv('telegram_token')
 AZURE_TRANSLATOR_KEY = os.getenv('AZURE_TRANSLATOR_KEY')
 AZURE_TRANSLATOR_REGION = os.getenv('AZURE_TRANSLATOR_REGION')
@@ -34,13 +33,9 @@ OPENAI_DEPLOYMENT_NAME = os.getenv('OPENAI_DEPLOYMENT_NAME')
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = False if in production
 DEBUG = True
 
 ALLOWED_HOSTS = []
